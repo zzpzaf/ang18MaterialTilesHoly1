032e6922075ffcff3d052abfef4e22d2868a1e98
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-08-29 19:04:34 +0300

    First mat-grid-list / mat-grid-tiles implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,7 @@ import { LeftpaneComponent } from './leftpane/leftpane.component';
 import { MainComponent } from './main/main.component';
 import { RightpaneComponent } from './rightpane/rightpane.component';
 import { FooterComponent } from './footer/footer.component';
-
-interface LayoutConfig {
-  [key: string]: { value: number | string; unit: string };
-}
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
@@ -21,42 +18,14 @@ interface LayoutConfig {
     MainComponent,
     RightpaneComponent,
     FooterComponent,
-    RouterOutlet
+    RouterOutlet,
+    MatGridListModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Angular18-Holy-Grail-repo1(Grid)';
+  
+  title = 'Angular18-Holy-Grail-With-Material-Grid-List)';
 
-  private layoutConfig: LayoutConfig = {
-    allPadding: { value: 6, unit: 'px' },
-
-    hhSmall: { value:65, unit: 'px' },
-    nrhSmall: { value: 'min-content', unit: '' },
-    lphSmall: { value: 1, unit: 'fr' },
-    mhSmall: { value: 1.7, unit: 'fr' },
-    rphSmall: { value:'min-content', unit: '' },
-    fhSmall: { value: 'min-content', unit: '' },
-
-    hhMedium: { value:70, unit: 'px' },
-    nrhMedium: { value: 40, unit: 'px' },
-    rphMedium: { value:'min-content', unit: '' },
-    fhMedium: { value: 50, unit: 'px' },
-    lpw1Medium: { value: 180, unit: 'px' },
-    lpw2Medium: { value: 20, unit: '%' },
-
-    hhLarge: { value:80, unit: 'px' },
-    nrhLarge: { value: 50, unit: 'px' },
-    fhLarge: { value: 60, unit: 'px' },
-    lpw1Large: { value: 200, unit: 'px' },
-    lpw2Large: { value: 25, unit: '%' },
-    rpwLarge: { value: 'min-content', unit: '' }
-  };
-
-  get layoutConfigStyle(): string {
-    return Object.entries(this.layoutConfig)
-      .map(([key, { value, unit }]) => `--${key}: ${value}${unit}`)
-      .join(';');
-  }
-}
+ }
