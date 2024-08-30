2f3d2d59311dfa2af0b1e71eaee43a4a0d9cbf92
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-08-30 13:32:49 +0300

    Dynamic components within mat-grid-list tiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Type } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-// import { HeaderComponent } from './header/header.component';
-// import { NavrowComponent } from './navrow/navrow.component';
-// import { LeftpaneComponent } from './leftpane/leftpane.component';
-// import { MainComponent } from './main/main.component';
-// import { RightpaneComponent } from './rightpane/rightpane.component';
-// import { FooterComponent } from './footer/footer.component';
+import { AddCompDynDirective } from './add-comp-dyn.directive';
+import { HeaderComponent } from './header/header.component';
+import { NavrowComponent } from './navrow/navrow.component';
+import { LeftpaneComponent } from './leftpane/leftpane.component';
+import { MainComponent } from './main/main.component';
+import { RightpaneComponent } from './rightpane/rightpane.component';
+import { FooterComponent } from './footer/footer.component';
 
 export interface Tile {
   cols: number;
@@ -16,17 +17,34 @@ export interface Tile {
   color: string;
 }
 
+interface DynLayOutComponentsType {
+  [key: string]: Type<any>;
+}
+
+export const DynLayOutComponents: DynLayOutComponentsType = {
+  header: HeaderComponent,
+  navrow: NavrowComponent,
+  leftpane: LeftpaneComponent,
+  main: MainComponent,
+  rightpane: RightpaneComponent,
+  footer: FooterComponent,
+};
+
+
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     MatGridListModule,
-    // HeaderComponent,
-    // NavrowComponent,
-    // LeftpaneComponent,
-    // MainComponent,
-    // RightpaneComponent,
-    // FooterComponent,
+    HeaderComponent,
+    NavrowComponent,
+    LeftpaneComponent,
+    MainComponent,
+    RightpaneComponent,
+    FooterComponent,
+    AddCompDynDirective,
     RouterOutlet,
   ],
   templateUrl: './app.component.html',
@@ -41,30 +59,30 @@ export class AppComponent implements OnInit {
   private currentBreakpoint: string = '';
 
   private tilesLarge: Tile[] = [
-    { text: 'Tile 1', cols: 12, rows: 1, color: 'dodgerblue' },
-    { text: 'Tile 2', cols: 12, rows: 1, color: 'lightblue' },
-    { text: 'Tile 3', cols: 2, rows: 8, color: 'lightpink' },
-    { text: 'Tile 4', cols: 9, rows: 8, color: 'lightgray' },
-    { text: 'Tile 5', cols: 1, rows: 8, color: 'lightgoldenrodyellow' },
-    { text: 'Tile 6', cols: 12, rows: 1, color: 'lightseagreen' },
+    { text: 'header', cols: 12, rows: 2, color: 'dodgerblue' },
+    { text: 'navrow', cols: 12, rows: 2, color: 'lightblue' },
+    { text: 'leftpane', cols: 2, rows: 16, color: 'lightpink' },
+    { text: 'main', cols: 9, rows: 16, color: 'lightgray' },
+    { text: 'rightpane', cols: 1, rows: 16, color: 'lightgoldenrodyellow' },
+    { text: 'footer', cols: 12, rows: 2, color: 'lightseagreen' },
   ];
 
   private tilesMedium: Tile[] = [
-    { text: 'Tile 1', cols: 12, rows: 1, color: 'dodgerblue' },
-    { text: 'Tile 2', cols: 12, rows: 1, color: 'lightblue' },
-    { text: 'Tile 3', cols: 2, rows: 7, color: 'lightpink' },
-    { text: 'Tile 4', cols: 10, rows: 7, color: 'lightgray' },
-    { text: 'Tile 5', cols: 12, rows: 1, color: 'lightgoldenrodyellow' },
-    { text: 'Tile 6', cols: 12, rows: 1, color: 'lightseagreen' },
+    { text: 'header', cols: 12, rows: 2, color: 'dodgerblue' },
+    { text: 'navrow', cols: 12, rows: 2, color: 'lightblue' },
+    { text: 'leftpane', cols: 2, rows: 14, color: 'lightpink' },
+    { text: 'main', cols: 10, rows: 14, color: 'lightgray' },
+    { text: 'rightpane', cols: 12, rows: 2, color: 'lightgoldenrodyellow' },
+    { text: 'footer', cols: 12, rows: 2, color: 'lightseagreen' },
   ];
 
   private tilesSmall: Tile[] = [
-    { text: 'Tile 1', cols: 12, rows: 1, color: 'dodgerblue' },
-    { text: 'Tile 2', cols: 12, rows: 1, color: 'lightblue' },
-    { text: 'Tile 3', cols: 12, rows: 2, color: 'lightpink' },
-    { text: 'Tile 4', cols: 12, rows: 5, color: 'lightgray' },
-    { text: 'Tile 5', cols: 12, rows: 1, color: 'lightgoldenrodyellow' },
-    { text: 'Tile 6', cols: 12, rows: 1, color: 'lightseagreen' },
+    { text: 'header', cols: 12, rows: 3, color: 'dodgerblue' },
+    { text: 'navrow', cols: 12, rows: 2, color: 'lightblue' },
+    { text: 'leftpane', cols: 12, rows: 4, color: 'lightpink' },
+    { text: 'main', cols: 12, rows: 10, color: 'lightgray' },
+    { text: 'rightpane', cols: 12, rows: 2, color: 'lightgoldenrodyellow' },
+    { text: 'footer', cols: 12, rows: 2, color: 'lightseagreen' },
   ];
 
   ngOnInit(): void {
